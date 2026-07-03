3f448552cda729e3b0a516d9532047417e178dbf
Giwi <marin.xavier@gmail.com>
2026-07-03 23:08:30 +0200

    refactor: replace moment with dayjs via esbuild alias

--- src/www/js/vendor.js
+++ src/www/js/vendor.js
@@ -2,8 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import $ from 'jquery'
 import 'bootstrap'
 import Chart from 'chart.js'
-import moment from 'moment'
+
+// ponytail: dayjs aliased as moment (2KB vs 230KB), satisfies Chart.js time scale
+import dayjs from 'dayjs'
 
 window.$ = window.jQuery = $
 window.Chart = Chart
-window.moment = moment
+window.moment = dayjs
