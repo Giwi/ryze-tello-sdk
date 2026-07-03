20819ce6cdcbb996ee6fb537d8c5c88051cd546a
Giwi <marin.xavier@gmail.com>
2026-07-03 22:35:43 +0200

    refactor: replace Bower with ESBuild

---
+++ src/www/js/vendor.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import $ from 'jquery'
+import 'bootstrap'
+import Chart from 'chart.js'
+import moment from 'moment'
+
+window.$ = window.jQuery = $
+window.Chart = Chart
+window.moment = moment
